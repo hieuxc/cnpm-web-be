f3316a52f64951bfda1fe5698b2e4158086ebb34
hieu phan <hieupq@ayainnovation.com>
2021-06-10 23:19:24 +0700

    fix

--- config/env/production.js
+++ config/env/production.js
@@ -252,10 +252,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://cnpm-web-be.herokuapp.com',
+    ],
 
 
     /***************************************************************************
